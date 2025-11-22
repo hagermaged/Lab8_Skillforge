dd20c032f3e8ec9425620121c44c183bae64357d
JanaNafei <jananafei103@gmail.com>
2025-11-23 01:29:56 +0200

    model classes updated

--- src/main/java/model/Lesson.java
+++ src/main/java/model/Lesson.java
@@ -16,6 +16,8 @@ public class Lesson {
     private String lessonTitle;
     private String content;
     private List<String> resources;
+    private Quiz quiz;
+    private boolean isCompleted;
     
     //constructor 1 - without resources
     public Lesson(String lessonId, String lessonTitle, String content){
@@ -32,7 +34,25 @@ public class Lesson {
         this.content = content;
         this.resources = resources;
     }
+    //constructor 3 - with quiz
+    public Lesson(String lessonId, String lessonTitle, String content, Quiz quiz){
+        this.lessonId = lessonId;
+        this.lessonTitle = lessonTitle;
+        this.content = content;
+        this.resources = new ArrayList<>();
+        this.quiz = quiz; // Set the quiz
+        this.isCompleted = false; // Not completed by default
+    }
     
+    //constructor 4 - with resources and quiz
+    public Lesson(String lessonId, String lessonTitle, String content, List<String> resources, Quiz quiz){
+        this.lessonId = lessonId;
+        this.lessonTitle = lessonTitle;
+        this.content = content;
+        this.resources = resources;
+        this.quiz = quiz; // Set the quiz
+        this.isCompleted = false; // Not completed by default
+    }
     //setters and getters
     public void setLessonId(String lessonId){
         this.lessonId = lessonId;
@@ -58,12 +78,29 @@ public class Lesson {
     public List<String> getResources(){
         return this.resources;
     }
+    public Quiz getQuiz() {
+        return quiz;
+    }
     
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+    
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
     //add or remove resource
     public void addResource(String resource){
         this.resources.add(resource);
     }
     public void removeResource(String resource){
         this.resources.remove(resource);
+    }
+      public boolean hasQuiz() {
+        return quiz != null;
     }
 }
--- src/main/java/model/Question.java
+++ src/main/java/model/Question.java
@@ -4,10 +4,72 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author orignal store
  */
 public class Question {
-    
+   private String questionId;
+    private String questionText;
+    private List<String> options; // mcq options
+    private int correctOptionIndex; // 0-based index (0, 1, 2, 3)
+    private int points;
+  public Question() {
+        this.options = new ArrayList<>();
+    }
+  
+    public Question(String questionId, String questionText, List<String> options, int correctOptionIndex, int points) {
+        this.questionId = questionId;
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+        this.points = points;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public int getCorrectOptionIndex() {
+        return correctOptionIndex;
+    }
+
+    public void setCorrectOptionIndex(int correctOptionIndex) {
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    //Helper method to check if answer is correct
+     public boolean isCorrectAnswer(int selectedIndex) {
+        return selectedIndex == correctOptionIndex;
+    }
 }
