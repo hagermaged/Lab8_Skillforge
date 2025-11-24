8ac77186d354b2b2d0b03fbcccc24a0cb8fde325
basmalamohamed938-hash <basmalamohamed938@gmail.com>
2025-11-24 06:36:25 +0200

    .

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+
+import javax.swing.SwingUtilities;
+
+import gui.AnalyticsFrame;
+
+public class main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AnalyticsFrame frame = new AnalyticsFrame();
+            frame.setVisible(true);
+        });
+    }
+}
